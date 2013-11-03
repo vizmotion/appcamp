7d2ac48c8571637256335eaef997fb46508977c8
Devin Weaver <suki@tritarget.org>
2013-11-03 12:16:33 -0500

    Zero based indexes for stories

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,8 +15,8 @@ function createStory(id) {
 }
 setTimeout(function(param) {
 	var stories = [];
-	stories.push(createStoryEntry(1,'And How My Spirit Soars', 'Story about women coming of age', '/spiritsoars.jpg'));
-	stories.push(createStoryEntry(2,'Fox and Hound', 'Fox makes friend with Hound', '/foxhound.jpg'));
+	stories.push(createStoryEntry(0,'And How My Spirit Soars', 'Story about women coming of age', '/spiritsoars.jpg'));
+	stories.push(createStoryEntry(1,'Fox and Hound', 'Fox makes friend with Hound', '/foxhound.jpg'));
 	
 	Ti.API.info('got stories: ' + JSON.stringify(stories));
 	//Alloy.Globals.stories = stories;
